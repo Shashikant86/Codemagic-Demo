dec5eaa61b5ad421f553bf993d084615ce4cf979
Shashikant86 <shashikant.jagtap@icloud.com>
2019-04-19 19:41:24 +0100

    Update Codemagic script

--- codemagic_firebase.sh
+++ codemagic_firebase.sh
@@ -2,6 +2,8 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
 
 brew install base64
-echo "===Base64 Installed Successfully"
 
+echo "===Base64 Installed Successfully"
+echo "==Decodeding GoogleService-Info.plist File"
 echo $IOS_FIREBASE |  /usr/local/Cellar/base64/1.5/bin/base64 --decode > $FCI_BUILD_DIR/ios/GoogleService-Info.plist
+echo "==Successfully Decoded GoogleService-Info.plist File"
