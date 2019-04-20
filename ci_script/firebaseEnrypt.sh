b3dea711b247c57aeb0e065de8f8b6a73c63c98a
Shashikant86 <shashikant.jagtap@icloud.com>
2019-04-20 08:08:31 +0100

    Add encrypted File

--- ci_script/firebaseEnrypt.sh
+++ ci_script/firebaseEnrypt.sh
@@ -2,8 +2,8 @@
 
 # Run This command from local machine 
 
-# openssl enc -aes-256-cbc -salt -in ios/GoogleService-Info.plist -out ios/GoogleService-Info.enc -k "YOUR_SECRET_PASSWORD"
+# openssl enc -aes-256-cbc -salt -in ios/Runner/GoogleService-Info.plist -out ios/GoogleService-Info.enc -k "YOUR_SECRET_PASSWORD"
 
 # Run this command on Codemagic after you set the environment variable
 
-# openssl enc -aes-256-cbc -d -in ios/GoogleService-Info.enc -out ios/GoogleService-Info.plist -k "YOUR_SECRET_PASSWORD_FROM_ENV_VARIABLE"
+# openssl enc -aes-256-cbc -d -in ios/Runner/GoogleService-Info.enc -out ios/Runner/GoogleService-Info.plist -k "YOUR_SECRET_PASSWORD_FROM_ENV_VARIABLE"
